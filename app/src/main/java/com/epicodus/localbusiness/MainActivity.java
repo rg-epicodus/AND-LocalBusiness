fc09564c984b3498b19eb42c63cf3d621df4f3f4
Rich Garrick <richg341@gmail.com>
2017-10-12 15:29:45 -0700

    add buttons to layout and style

--- app/src/main/java/com/epicodus/localbusiness/MainActivity.java
+++ app/src/main/java/com/epicodus/localbusiness/MainActivity.java
@@ -1,13 +1,27 @@
 package com.epicodus.localbusiness;
 
+
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.imageButton) Button mFindDrinksButton;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface rubikFont = Typeface.createFromAsset(getAssets(), "fonts/rubikregular.ttf");
+        mAppNameTextView.setTypeface(rubikFont);
+
     }
 }
